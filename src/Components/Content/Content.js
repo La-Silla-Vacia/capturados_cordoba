b184e87f84f8ce83bd6c50d5482160acd35cb4aa
Wietse Neven <info@wn-multimedia.nl>
2017-05-09 23:31:21 -0500

    Added a fadeout animation on the content

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -31,7 +31,9 @@ export default class Content extends Component {
     this.state = {
       height: 0,
       visible: false
-    }
+    };
+
+    this.handleClose = this.handleClose.bind(this);
   }
 
   componentDidMount() {
@@ -43,8 +45,15 @@ export default class Content extends Component {
     }, 100);
   }
 
+  handleClose() {
+    this.setState({visible: false});
+    setTimeout(() => {
+      this.props.toggleContentCallback(false);
+    }, 400);
+  }
+
   render(props, state) {
-    const { graphHeight, toggleContentCallback, name, description } = props;
+    const { graphHeight, name, description } = props;
     const { height, visible } = state;
 
     let style;
@@ -53,7 +62,7 @@ export default class Content extends Component {
         position: 'absolute',
         height: '100%'
       };
-      console.log(height, graphHeight);
+
       if (height > graphHeight) {
         style = {
           marginTop: -(graphHeight),
@@ -66,9 +75,10 @@ export default class Content extends Component {
 
     return (
       <div id="lsvi-content-box" className={cx(s.container, { [s.container__visible]: visible })} style={style}>
+        <div className={s.overlay} onClick={this.handleClose} />
         <div className={s.inner}>
           <div className={s.header}>
-            <button className={s.button} onClick={toggleContentCallback.bind(this, false)}>
+            <button className={s.button} onClick={this.handleClose}>
               <svg className={s.arrow} x="0px" y="0px"
                    viewBox="0 0 95.9 65.9" style="enable-background:new 0 0 95.9 65.9;" xmlSpace="preserve">
                 <path className={s.arrowPath} d="M90.6,1.5C72.1,23,44.9,18.1,44.9,18.1V2.9c0-1.1-0.6-2.1-1.6-2.6c-1-0.5-2.2-0.4-3.1,0.3L1.9,29.4C0.7,30.2,0,31.6,0,33.1
@@ -81,7 +91,7 @@ export default class Content extends Component {
           <h3 className={s.title}>{name}</h3>
           <div dangerouslySetInnerHTML={{ __html: content }} />
 
-          <button className={cx(s.button, s.button__end)} onClick={toggleContentCallback.bind(this, false)}>
+          <button className={cx(s.button, s.button__end)} onClick={this.handleClose}>
             <svg className={s.arrow} x="0px" y="0px"
                  viewBox="0 0 95.9 65.9" style="enable-background:new 0 0 95.9 65.9;" xmlSpace="preserve">
               <path className={s.arrowPath} d="M90.6,1.5C72.1,23,44.9,18.1,44.9,18.1V2.9c0-1.1-0.6-2.1-1.6-2.6c-1-0.5-2.2-0.4-3.1,0.3L1.9,29.4C0.7,30.2,0,31.6,0,33.1
